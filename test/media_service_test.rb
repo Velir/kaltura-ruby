f0f0bb770d198f53e5566976dfaccd3e57f037dd
prabode <prabode@gmail.com>
2011-03-17 11:24:34 -0400

    enhanced the media service tests

--- test/media_service_test.rb
+++ test/media_service_test.rb
@@ -7,12 +7,9 @@ class MediaServiceTest < Test::Unit::TestCase
       media_entry = Kaltura::MediaEntry.new
    	  media_entry.media_type = Kaltura::Constants::Media::Type::VIDEO
    	  video_file = File.open("test/media/test.mov")
-   	  begin
-   	       video_token = @client.media_service.upload(video_file)
-   	       @created_entry = @client.media_service.add_from_uploaded_file(media_entry,video_token)
-   	       assert_not_nil @created_entry
-   	  rescue Kaltura::APIError => e
-   	  end
+   	  video_token = @client.media_service.upload(video_file)
+   	  created_entry = @client.media_service.add_from_uploaded_file(media_entry,video_token)
+   	  assert_not_nil created_entry.id
    end
    
    # test delete entry 
@@ -21,12 +18,8 @@ class MediaServiceTest < Test::Unit::TestCase
      media_entry = Kaltura::MediaEntry.new
   	  media_entry.media_type = Kaltura::Constants::Media::Type::VIDEO
   	  video_file = File.open("test/media/test.mov")
-  	  begin
-  	       video_token = @client.media_service.upload(video_file)
-  	       @created_entry = @client.media_service.add_from_uploaded_file(media_entry,video_token)
-  	       assert_nil @client.media_service.delete(@created_entry)
-  	  rescue Kaltura::APIError => e
-  	  end
-      
+  	  video_token = @client.media_service.upload(video_file)
+  	  created_entry = @client.media_service.add_from_uploaded_file(media_entry,video_token)
+  	  assert_nil @client.media_service.delete(created_entry.id)
     end
 end
